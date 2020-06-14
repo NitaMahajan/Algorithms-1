491e8a8b7134186fc129e5da05a2f8983c1cce0f
Nita Mahajan <nitamahajan.14@gmail.com>
2020-06-14 13:04:03 +0530

    weighted-union

--- src/QuickUnionRunner.java
+++ src/QuickUnionRunner.java
@@ -7,20 +7,53 @@ class QuickUnion{
 		}
 	}
 	public boolean connected(int x, int y) {
-		return (ar[x]==ar[y]);
+		return (root(x)==root(y));
 	}
 	int root(int x) {
-		int i=x;
-		while(ar[i]!=i){
-			i=ar[i];
+		while(ar[x]!=x){
+			x=ar[x];
 		}
-		return i;
+		return x;
 	}
 	public void union(int x, int y) {
 		int rootx = root(x);
 		int rooty = root(y);
 		ar[rootx] = rooty;
 	}
+}
+class WeightedQuickUnion{
+	int ar[];
+	int sz[];
+	WeightedQuickUnion(int N){
+		ar = new int[N];
+		for(int i = 0; i < N; i++){
+			ar[i] = i;
+		}
+		sz = new int[N];
+	}
+	int root(int x){
+		while(ar[x] != x){
+			x = ar[x];
+		}
+		return x;
+	}
+	public boolean connected(int x, int y) {
+		return (root(x) == root(y));
+	}
+	public void union(int x, int y) {
+		int rootx = root(x);
+		int rooty = root(y);
+		if(rootx == rooty) return;
+		if(sz[rootx] < sz[rooty]) {
+			ar[rootx] = ar[rooty];
+			sz[rootx] = sz[rootx] + sz[rooty];
+		}
+		else {
+			ar[rooty] = ar[rootx];
+			sz[rooty] = sz[rooty] + sz[rootx];
+		}
+	}
+	
 }
 public class QuickUnionRunner {
 
@@ -38,6 +71,21 @@ public class QuickUnionRunner {
 		u.union(7, 2);
 		u.union(6, 1);
 		System.out.println(u.connected(0, 7));
+		
+		WeightedQuickUnion wu = new WeightedQuickUnion(10);
+		wu.union(4,3);
+		wu.union(3,8);
+		wu.union(6, 5);
+		wu.union(9, 4);
+		wu.union(2, 1);
+		System.out.println(wu.connected(0, 7));
+		System.out.println(wu.connected(8, 9));
+		wu.union(2, 1);
+		wu.union(5, 0);
+		wu.union(7, 2);
+		wu.union(6, 1);
+		System.out.println(wu.connected(0, 7));
+		
 	}
 
 }
